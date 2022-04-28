9e298dd05fd37e20e98eb2cd88fc5284b92906f1
Kirsten tay <taykirsten11@gmail.com>
2022-04-28 23:25:58 +0000

    updated the dashboard

--- src/layouts/dashboardLayout.jsx
+++ src/layouts/dashboardLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import {BellIcon}from '@heroicons/react/outline'
 
 
 const MainLayout = () => {
   return (
     <div>
        <nav className="bg-gray-800 w-full h-full">
-       <div class="flex items-center  p-14">
+       <div class="flex items-center  h-16  justify-between  ">
+         <div>
 
        <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" >Dashboard</a>
 
@@ -15,8 +17,38 @@ const MainLayout = () => {
      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+</div>
 
-          </div>
+          <div class="flex">
+          <div class="hidden md:block">
+          <div class=" flex items-center ">
+            <button type="button" class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <span  class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" />
+            </button>
+            </div>
+            </div>
+
+            <div class=" relative">
+              <div>
+                <button type="button" class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                  <span class="sr-only">Open user menu</span>
+                  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                </button>
+              </div>
+
+              <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+             
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+              </div>
+
+              </div>
+              </div>
+</div>
       </nav>
     </div>
   )
