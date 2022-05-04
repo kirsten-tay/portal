c15d7de433f54d9bfd1db5348a81b3103e21ed4f
Kirsten tay <taykirsten11@gmail.com>
2022-05-04 18:24:45 +0000

    updated the dashboard

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const dashboard = () => {
   return (
-    <div>
+    <div className="border-2 rounded-md">
+      <h2 className="text-black font-medium">DASHBOARD.</h2>
       
     </div>
   )
