3053fd8d26db0b29e1a2c014dcf0c39957a558b0
Kirsten tay <taykirsten11@gmail.com>
2022-05-06 18:01:26 +0000

    updated the dashboard

--- src/layouts/dashboardLayout.jsx
+++ src/layouts/dashboardLayout.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import Add from './../components/add'
-import {BellIcon}from '@heroicons/react/outline'
+import {BellIcon, ChevronDownIcon}from '@heroicons/react/outline'
 import { ProSidebar, Menu,MenuItem, SubMenu } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
 import Profilebutton from './../components/profilebutton'
 import Portal from '../components/Portal';
 
@@ -27,11 +26,11 @@ const dashboardLayout = () => {
    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
 </div>
 
-           <div class="relative inline-block text-left">
+          <div class="flex space-x-3">
           <div class="hidden md:block">
-          <div class=" flex items-center ">
+          <div class=" flex items-center space-x-3 ">
              <Add/>
-            <button type="button" class="inline-flex justify-center w-full  bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            <button type="button" class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
               <span  class="sr-only">View notifications</span>
               <BellIcon class="h-6 w-6" />
             </button>
@@ -39,8 +38,11 @@ const dashboardLayout = () => {
             </div>
            
 
-            
-          
+            <div class="relative inline-block text-left">
+            <div className="flex items-center">
+            <Profilebutton type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true"/>
+          <ChevronDownIcon className="h-3 w-3"/>
+               </div>
             <div class="origin-top-right absolute right-0 mt-2 w-32 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
           <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Profile </a>
@@ -52,12 +54,13 @@ const dashboardLayout = () => {
         </div>
            </div>
             
+            </div>
           
 </div>
       </nav>
       <div >
-       <div class="flex items-center  justify-between bg-slate-600  ">
-      <ProSidebar className="flex h-full bg-slate-600 " >
+       <div class="flex items-center  origin-left justify-between bg-slate-600  ">
+      <ProSidebar className="flex overflow-y-auto h-full bg-slate-600 " >
       <Menu iconShape="square" className="flex space-y-5" >
       <MenuItem icon={<BellIcon/>}>Dashboard</MenuItem>
       <MenuItem icon={<Portal/>}/>
